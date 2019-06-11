95bb689c7fb28d12a6a1f52bd18966a620581dca
Gustavo Ocanto <gustavoocanto@gmail.com>
2019-06-11 17:23:33 +0800

    kick off tests

--- src/ExporterInterface.php
+++ src/ExporterInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Gocanto Laravel-Simple-PDF package
+ *
+ * (c) Gustavo Ocanto <gustavoocanto@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gocanto\SimplePDF;
 
 use Psr\Http\Message\StreamInterface;
---
+++ tests/BuilderTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Gocanto Laravel-Simple-PDF package
+ *
+ * (c) Gustavo Ocanto <gustavoocanto@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gocanto\SimplePDF\Tests;
+
+use Gocanto\SimplePDF\Builder;
+use Gocanto\SimplePDF\ExporterInterface;
+use Illuminate\Contracts\View\Factory;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase
+{
+    /** @var ExporterInterface */
+    private $exporter;
+    /** @var Factory */
+    private $renderer;
+    /** @var Builder */
+    private $builder;
+
+    protected function setUp() : void
+    {
+        $this->exporter = Mockery::mock(ExporterInterface::class);
+        $this->renderer = Mockery::mock(Factory::class);
+        $this->renderer->shouldReceive('addLocation');
+        $this->builder = new Builder($this->exporter, $this->renderer);
+    }
+
+    protected function tearDown() : void
+    {
+        Mockery::close();
+    }
+
+    /** @test */
+    public function it_makes_the_proper_voucher()
+    {
+        $this->renderer->shouldReceive('make')->once()->andReturn('content');
+        $this->exporter->shouldReceive('addContent')->once();
+        $this->exporter->shouldReceive('export')->once();
+
+        $this->builder->make(['foo' => 'bar']);
+    }
+}
